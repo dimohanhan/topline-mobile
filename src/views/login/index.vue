eb943408e76e9caadf1e1d65dbc1e4564e0aa260
dimohan <13104196063@163.com>
2019-07-09 16:20:53 +0800

    登录页面布局及信息挂载完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,29 @@
 <template>
     <div class="login">
 <van-nav-bar
-    title="目录"
+    title="登录"
 />
+<van-cell-group>
+  <van-field
+    v-model="user.mobile"
+    required
+    clearable
+    label="手机号"
+    placeholder="请输入手机号"
+  />
+
+  <van-field
+    v-model="user.code"
+    type="password"
+    label="验证码"
+    placeholder="请输入验证码"
+    required
+  />
+</van-cell-group>
+<div class="login-btn">
+    <van-button class="btn" type="info">登录</van-button>
+</div>
+
     </div>
 </template>
 
@@ -11,7 +32,10 @@ export default {
   name: 'Applogin',
   data () {
     return {
-
+      user: {
+        mobile: '',
+        code: ''
+      }
     }
   }
 }
@@ -19,7 +43,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.login {
-    background-color: skyblue
+.van-nav-bar  {
+    background-color: rgb(63, 167, 252)
+}
+.van-icon {
+    width: 20px;
+    height: 20px;
+}
+.login-btn {
+    padding:35px;
+    .btn {
+        width: 100%
+    }
 }
 </style>
